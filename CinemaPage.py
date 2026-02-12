20503bd4c0e99fc7eb7a884243238e391baab4c8
ddimonoff <ddimonoff@yandex.ru>
2026-02-12 21:59:17 +0400

    readme editted

--- CinemaPage.py
+++ CinemaPage.py
@@ -12,31 +12,60 @@ class CinemaPage:
                 resp = requests.get(f'{self.base_url}v1.4/movie/{id}', headers=self.my_headers)
                 with allure.step(f"Проверка статус кода {resp.status_code}"):
                     if resp.status_code == 200:
-                        with allure.step("Проверка названия фильма"):
-                            var = resp.json()["name"]
-                            assert var == "Побег"
+                        with allure.step(f"Проверка названия фильма {resp.json()["name"]}"):
+                            return resp.json()["name"]
                     elif resp.status_code == 404:
                         print("По этому id ничего не найдено!")
                         return None
                     else:
                         print(f"Ошибка API: {resp.status_code}")
-            except requests.exceptions.RequestException as e:
-                print(f"Другая ошибка: {e}")
+            except requests.exceptions.RequestException:
+                pass
 
 
-    def get_cinema(self, cinema_name):
-        with allure.step(f"Запрос фильма по названию - {cinema_name}"):
+    def get_cinema(self, cinema_name, ref_id):
+        with (allure.step(f"Запрос фильма по названию - {cinema_name}")):
             params = {
                 "page": 1,
                 "limit": 10,
                 "query": cinema_name
             }
-            resp = requests.get(f'{self.base_url}v1.4/movie/search',headers=self.my_headers, params=params)
-        with allure.step("Проверка статус кода 200"):
-            assert resp.status_code == 200
-        with allure.step(f"Проверка ID фильма {cinema_name}"):
-            cinema_id = resp.json()
-            list_id = []
-            for movie in cinema_id.get("docs", []):
-                list_id += [movie.get('id')]
-                return list_id
+            try:
+                resp = requests.get(f'{self.base_url}v1.4/movie/search',headers=self.my_headers, params=params)
+                with allure.step(f"Проверка статус кода {resp.status_code}"):
+                    if resp.status_code == 200:
+                        with allure.step(f"Проверка ID фильма {cinema_name}"):
+                            cinema_id = resp.json()
+                            list_id = []
+                        for movie in cinema_id.get("docs", []):
+                            list_id += [movie.get('id')]
+                        for a in list_id:
+                            if a == int(ref_id):
+                                with allure.step(f"ID фильма = {a}"):
+                                    return a  # сразу возвращаем
+                        return None  # если ничего не нашли
+                    elif resp.status_code == 404:
+                        print("По этому названию ничего не найдено!")
+                        return None
+                    else:
+                        print(f"Ошибка API: {resp.status_code}")
+            except requests.exceptions.RequestException:
+                pass
+
+
+    def get_person(self, id):
+        with allure.step("Поиск персоны по ID " + id):
+            try:
+                resp = requests.get(f'{self.base_url}v1.4/person/{id}', headers=self.my_headers)
+                with allure.step(f"Проверка статус кода {resp.status_code}"):
+                    if resp.status_code == 200:
+                        with allure.step(f"Проверка фамилии {resp.json()["name"]}"):
+                            return resp.json()["name"]
+                    elif resp.status_code == 404:
+                        print("По этому id ничего не найдено!")
+                        return None
+                    else:
+                        print(f"Ошибка API: {resp.status_code}")
+            except requests.exceptions.RequestException:
+                pass
+
--- test_API.py
+++ test_API.py
@@ -10,19 +10,41 @@ my_headers = {
 }
 kino = CinemaPage(base_url, my_headers)
 
-
-@allure.title("Расширенный поиск фильмов")
+#  final_project_kinopoisk
+#  ./run.sh
+@allure.title("Поиск")
 @allure.severity("blocker")
 @allure.feature("READ")
-@allure.description("Тест проверяет работу "
-                    "расширенного поиска")
+@allure.description("Тест проверяет работу окна поиска")
 @allure.epic("Кинопоиск")
 def test_API() -> None:
-    id = "533128"  # 1091
-    kino.get_kino(id)
     id1 = "1091"
-    list_id = kino.get_cinema("Люди в чёрном")
-    assert list_id[0] == int(id1)
-    id2 = "1722"
-    list_id = kino.get_cinema("Twister")
-    assert list_id[0] == int(id2)
+    ref_name1 = "Люди в чёрном"
+    with (allure.step("Запрос фильма по ID")):
+        actual_name =kino.get_kino(id1)
+    assert actual_name == ref_name1
+    id2 = "533128"
+    ref_name2 = "Побег"
+    with (allure.step("Запрос фильма по названию на кириллице")):
+        actual_id2 = kino.get_cinema(ref_name2, id2)
+    assert actual_id2 == int(id2)
+    id3 = "1722"
+    ref_name3 = "Twister"
+    with (allure.step("Запрос фильма по названию на латинице")):
+        actual_id3 = kino.get_cinema(ref_name3, id3)
+    assert actual_id3 == int(id3)
+    id4 = "840744"
+    ref_name4 = "Тихий Дон"
+    with (allure.step("Запрос фильма по названию с пробелом")):
+        actual_id4 = kino.get_cinema(ref_name4, id4)
+    assert actual_id4 == int(id4)
+    id5 = "43949"
+    ref_name5 = "Спортлото-82"
+    with (allure.step("Запрос фильма по названию с цифрами")):
+        actual_id5 = kino.get_cinema(ref_name5, id5)
+    assert actual_id5 == int(id5)
+    id6 = "22260"
+    ref_name6 = "Стивен Спилберг"
+    with (allure.step("Запрос актёров/режиссеров по ID")):
+        actual_name = kino.get_person(id6)
+    assert actual_name == ref_name6
